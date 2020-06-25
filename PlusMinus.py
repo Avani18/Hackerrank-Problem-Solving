078458390ffd3aed81f538036474c06353d6c7dd
Avani Jindal <33371063+Avani18@users.noreply.github.com>
2020-06-26 02:40:12 +0530

    Add files via upload

---
+++ PlusMinus.py
@@ -0,0 +1,20 @@
+
+def plusMinus(arr):
+    pos = 0
+    neg = 0
+    zero = 0
+    for i in arr:
+        if i > 0:
+            pos += 1
+        elif i < 0:
+            neg += 1
+        else:
+            zero += 1
+    print (pos/len(arr))
+    print (neg/len(arr))
+    print (zero/len(arr))
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().rstrip().split()))
+    plusMinus(arr)
